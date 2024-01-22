6f949b4cce7f05d8eb9ea139d8e28c4735e2d88c
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-01-22 10:58:14 +0100

    JDBC Lecture - Example 2 - Find By Id
    - Created a connection class
    - Create a PreparedStatement
    - Close Recourses.

--- jdbc-lecture/src/main/java/se/lexicon/JDBCExample.java
+++ jdbc-lecture/src/main/java/se/lexicon/JDBCExample.java
@@ -1,5 +1,6 @@
 package se.lexicon;
 
+import se.lexicon.db.MySQLConnection;
 import se.lexicon.model.Student;
 
 import java.sql.*;
@@ -10,21 +11,21 @@ import java.util.List;
 public class JDBCExample {
 
     public static void main(String[] args) {
-        ex1();
+        ex2();
     }
 
     // get All student data
-    public static void ex1(){
+    public static void ex1() {
 
         List<Student> students = new ArrayList<>();
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/school_db", "root", "1234");
+            Connection connection = MySQLConnection.getConnection();
 
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM students");
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 //ColumnLabel
                 int student_id = resultSet.getInt("student_id");
                 String firstName = resultSet.getString("first_name");
@@ -33,7 +34,7 @@ public class JDBCExample {
                 String email = resultSet.getString("email");
                 LocalDate createDate = resultSet.getDate("create_date").toLocalDate();
 
-                Student student = new Student(student_id,firstName,lastName,age,email,createDate);
+                Student student = new Student(student_id, firstName, lastName, age, email, createDate);
                 students.add(student);
             }
 
@@ -42,5 +43,47 @@ public class JDBCExample {
         }
 
         students.forEach(System.out::println);
+    }
+
+
+    //Get Student by Student_id
+    public static void ex2() {
+        // Step 1 : Create DB Connection
+        // Step 2 : Create a PreparedStatement
+        // Step 3 : Execute The Query
+        // Step 4 : Get Data From ResultSet and convert data to student Object.
+        // Step 5 : Close all the resources (try-with-recourse)
+
+        int findStudentById = 3;
+        Student student = null;
+
+        try (
+                Connection connection = MySQLConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM students WHERE student_id = ?");
+        ) {
+
+            preparedStatement.setInt(1, findStudentById);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+
+                if (resultSet.next()) {
+                    int student_id = resultSet.getInt("student_id");
+                    String firstName = resultSet.getString("first_name");
+                    String lastName = resultSet.getString("last_name");
+                    int age = resultSet.getInt("age");
+                    String email = resultSet.getString("email");
+                    LocalDate createDate = resultSet.getDate("create_date").toLocalDate();
+
+                    student = new Student(student_id, firstName, lastName, age, email, createDate);
+                }
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } // Will Close All Resources -> ResultSet, PreparedStatement, connection
+
+        System.out.println(student);
+
     }
 }
