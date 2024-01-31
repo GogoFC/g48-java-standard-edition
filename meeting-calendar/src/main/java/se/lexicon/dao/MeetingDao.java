c411d96d41410fdca2ae95de842f5b168a0d92c0
Goran M <goranmuminovic@gmail.com>
2024-01-31 10:18:34 +0100

    meedingDAo

--- meeting-calendar/src/main/java/se/lexicon/dao/MeetingDao.java
+++ meeting-calendar/src/main/java/se/lexicon/dao/MeetingDao.java
@@ -9,16 +9,16 @@ public interface MeetingDao {
 
     // TODO - Write Queries to perform the following behaviors
     //  Implement SQL query to insert a new meeting into the database
-    String CREATE_MEETING_SQL = "";
+    String CREATE_MEETING_SQL = "INSERT INTO MEETINGS (TITLE, START_TIME, END_TIME, _DESCRIPTION) VALUES (?,?,?,?)";
 
     // TODO: Implement SQL query to find a meeting by its ID in the database
-    String FIND_BY_ID_SQL = "";
+    String FIND_BY_ID_SQL = "SELECT * FROM MEETINGS WHERE ID = ?";
 
     // TODO: Implement SQL query to find all meetings by calendar ID in the database
-    String FIND_ALL_BY_CALENDAR_ID_SQL = "";
+    String FIND_ALL_BY_CALENDAR_ID_SQL = " SELECT * FROM MEETINGS WHERE CALENDAR_ID = ?";
 
     // TODO: Implement SQL query to delete a meeting by its ID from the database
-    String DELETE_MEETING_SQL = "";
+    String DELETE_MEETING_SQL = "DELETE FROM MEETINGS WHERE ID = ?";
 
 
     Meeting createMeeting(Meeting meeting);
